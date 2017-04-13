e8e8bef78b894ace7145b36348250e28d831ce4a
inakiam <heygehtq@gtiori.com>
2017-04-13 06:07:09 -0600

    Cruft removed from eqs in preparation for rewrite.

--- eqs.py
+++ eqs.py
@@ -30,155 +30,62 @@ def mandelbrot(z,c,pwr,itr,limit,j,transform):
 
     return z #Return coordinate vector of all zs
 
+#saku preloop
+p = z
+#saku loop
+zl = z
+z = (z*p*c) ** pwr + c
+p = zl
 
-#items under this line need to be updated to the new output format
 
-def sakura(z,c,pwr,itr,limit,j = False,ex = False,i=0):
 
-    '''Sakura Set ETA.'''
+#mBar
+z = z.conjugate() ** pwr + c
 
-    if j: z,c = c,z #Invert coordinate pairs for Julia
+#bShipppppp
+z = (abs(z.real)+abs(z.imag)*j) ** pwr + c
 
-    p = z
 
-    while abs(z) < limit and i != itr:
+#bSaku preloop
+p = z
+#bSaku Loop
+zl = z
 
-        i += 1
+z = (complex(abs(z.real),abs(z.imag))
+     *complex(abs(c.real),abs(c.imag))
+     *complex(abs(p.real),abs(p.imag))) ** pwr + c
 
-        zl = z
+p = zl
 
-        z = (z*p*c) ** pwr + c
 
-        p = zl
 
-    return z,i
 
-def mBar(z,c,pwr,itr,limit,j = False,ex = False,i=0):
+#sinBrot. limit should be multiplied by 3 for better deets
+z = cmath.sin(z) ** pwr + c
 
-    '''Mandelbar Set ETA.'''
 
-    if j: z,c = c,z #Invert coordinate pairs for Julia
 
-    while abs(z) < limit and i != itr:
 
-        i += 1
+#duck
+z = complex(z.real,abs(z.imag)) ** pwr + c
 
-        z = z.conjugate() ** pwr + c
 
-    return z,i
 
+#kel
+if i%2== 0: z = z ** pwr + c
+else:
 
-def bShip(z,c,pwr,itr,limit,j = False,ex = False,i=0):
+    if i % 3 == 0: z = z ** pwr / c
+    else: z = z ** pwr - c
 
-    '''Burning Ship ETA.'''
 
-    if j: z,c = c,z #Invert coordinate pairs for Julia
 
-    while abs(z) < limit and i != itr:
 
-        i += 1
+#apollyon
+z = (c/z) - z ** pwr + c
 
-        z = (abs(z.real)+abs(z.imag)*j) ** pwr + c
 
-    return z,i
 
-
-def bSaku(z,c,pwr,itr,limit,j = False,ex = False,i=0):
-
-    '''Burning Sakura ETA.'''
-
-    if j: z,c = c,z #Invert coordinate pairs for Julia
-
-    p = z
-
-    while abs(z) < limit and i != itr:
-
-        i += 1
-
-        zl = z
-
-        z = (complex(abs(z.real),abs(z.imag))
-             *complex(abs(c.real),abs(c.imag))
-             *complex(abs(p.real),abs(p.imag))) ** pwr + c
-
-        p = zl
-
-    return z,i
-
-def sinBrot(z,c,pwr,itr,limit,j = False,i = 0):
-
-    '''Sinebrot ETA.'''
-
-    if j: z,c = c,z #Invert coordinate pairs for Julia
-
-    limit *= 3 # Necessary for good detail due to sin.
-
-    while abs(z) < limit and i != itr:
-
-        i += 1
-
-        z = cmath.sin(z) ** pwr + c
-
-    return z,i
-
-def duck(z,c,pwr,itr,limit,j = False,ex = False,i=0):
-
-    '''Burning Ship ETA.'''
-
-    if j: z,c = c,z #Invert coordinate pairs for Julia
-
-    while abs(z) < limit and i != itr:
-
-        i += 1
-
-        z = complex(z.real,abs(z.imag)) ** pwr + c
-
-    return z,i
-
-class Formula(object):
-
-    z = 0
-    c = 0
-    pwr = 0
-    itr = 0
-    limit = 0
-    j = False
-    ex = False
-    i = 0
-
-def kel(z,c,pwr,itr,limit,j = False,ex = False,i=0):
-
-    '''Burning Ship ETA.'''
-
-    if j: z,c = c,z #Invert coordinate pairs for Julia
-
-    while abs(z) < limit and i != itr:
-
-        if i%2== 0: z = z ** pwr + c
-        else:
-
-            if i % 3 == 0: z = z ** pwr / c
-            else: z = z ** pwr - c
-
-
-        i += 1
-
-    return z,i
-
-
-def apollyon(z,c,pwr,itr,limit,j = False,ex = False,i=0):
-
-    '''Fractal with apollonian julia-packing.'''
-
-    if j: z,c = c,z #Invert coordinate pairs for Julia
-
-    while abs(z) < limit and i != itr:
-
-        z = (c/z) - z ** pwr + c
-        
-        i += 1
-
-    return z,i
     
 
 eqs = [mandelbrot,sakura,mBar,bShip,bSaku,sinBrot,duck,apollyon,kel]
