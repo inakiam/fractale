4afe54f90351abbf698a12fbbaf2cd09ba1925a7
inakiam <heygehtq@gtiori.com>
2017-06-01 23:43:39 -0600

    something

--- FractaleGUI/gui.py
+++ FractaleGUI/gui.py
@@ -1,14 +1,33 @@
 import kivy
-
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
+
+
+
+kivy.require("1.9.1")
+
+class About(Popup):
+    pass
+
+class Progress(Popup):
+    pass
 
 class NestedFloatLayout(FloatLayout):
+    def aboutP(self):
+        self._about = About()
+        self._about.open()
+
+    def progBar(self):
+        self._prog = Progress()
+        self._prog.open()
+
     pass
 
 
 class FractaleGUI(App):
 
+  
     def build(self):
         return NestedFloatLayout()
 
